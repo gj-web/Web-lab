d5ef7aabda1eb9fadf97022781af796a7f2b0116
Eva <129327174+gj-web@users.noreply.github.com>
2024-11-11 23:09:37 +0100

    Add files via upload

---
+++ Lab2_Veb/Web-Programming-master/lab/src/main/java/mk/finki/ukim/mk/lab/model/Location.java
@@ -0,0 +1,11 @@
+package mk.finki.ukim.mk.lab.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    private Long id;
+    private String address;
+}
